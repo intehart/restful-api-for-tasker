9010f5a1c9ec2e09c02fbdfc4ceed3964b44584d
Ruslan Zamalov <zamruma@gmail.com>
2020-09-13 19:53:15 +0300

    fix router not is Authenticated

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
+import {HomePage} from './pages/HomePage'
 import {AuthPage} from './pages/AuthPage';
 import {CreateTaskPage} from './pages/CreateTaskPage';
 import {TasksPage} from './pages/TasksPage';
 import {TaskDetailPage} from './pages/TaskDetailPage';
 
 export const useRouts = isAuthenticated => {
-    if (isAuthenticated) {
-        return (
-            <Switch>
-                <Route path="/tasks" exact>
-                    <TasksPage />
-                </Route>
-                <Route path="/task-detail/:id" exact>
-                    <TaskDetailPage />
-                </Route>
-                <Route path="/task-crate">
-                    <CreateTaskPage />
-                </Route>
-                <Redirect to="/task-crate" />
-            </Switch>
-        )
-    }
+  if (isAuthenticated) {
+    return (
+      <Switch>
+        <Route path="/tasks" exact>
+          <TasksPage />
+        </Route>
+        <Route path="/task-detail/:id" exact>
+          <TaskDetailPage />
+        </Route>
+        <Route path="/task-crate">
+          <CreateTaskPage />
+        </Route>
+        <Redirect to="/task-crate" />
+      </Switch>
+    )
+  }
 
     return (
-        <Switch>
-            <Route to="/" exact>
-                <AuthPage />
-            </Route>
-            <Redirect to="/" />
-        </Switch>
+      <div>
+        <Route path="/">
+          <HomePage />
+        </Route>
+        <Route path="/auth">
+          <AuthPage />
+        </Route>
+      </div>
     )
 }
