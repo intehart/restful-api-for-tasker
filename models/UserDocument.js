3315b34939f5b428c7fdc7f422ecb04081f56659
dasauser <dasauser@gmail.com>
2020-09-18 01:11:51 +0300

    implement load document and creating base record methods

--- models/UserDocument.js
+++ models/UserDocument.js
@@ -1,8 +1,15 @@
 'use strict';
 const { Model, DataTypes } = require('sequelize');
+const path = require('path');
+const crypto = require('crypto');
 
 module.exports = (sequelize) => {
   class UserDocument extends Model {
+
+    constructor(values) {
+      super(values);
+      this.fileResource = values.fileResource;
+    }
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -10,6 +17,27 @@ module.exports = (sequelize) => {
      */
     static associate(models) {
       // define association here
+    }
+
+    generateNewFileName() {
+      const time = new Date();
+
+      const fileExtension = path.extname(this.fileResource.name);
+
+      const hashName = crypto
+        .createHash('md5')
+        .update(`${this.fileResource.md5}${this.fileResource.name}${time.getTime()}${this.user_id}`)
+        .digest('hex');
+
+      const fileName = hashName + fileExtension;
+
+      this.file = `${__root}/public/files/${time.getFullYear()}/${time.getMonth()}/${time.getDay()}/${fileName}`;
+
+      return this;
+    }
+
+    uploadDocument() {
+      return this.fileResource.mv(this.file);
     }
   }
 
